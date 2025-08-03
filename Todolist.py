f93e2b935bb3c59f41011cf4f798738a0bc3f8d8
Janakiraman B <janakiraman171201@gmail.com>
2025-08-03 23:45:00 +0530

    Todolist.py

--- Todolist.py
+++ Todolist.py
@@ -11,4 +11,18 @@ def todo_list():
     for i, task in enumerate(tasks, 1):
         print(f"{i}. {task}")
 
+
 todo_list()
+
+
+#OUTPUT
+
+Enter a task (or 'quit' to stop): Reading
+Enter a task (or 'quit' to stop): learning
+Enter a task (or 'quit' to stop): studying
+Enter a task (or 'quit' to stop): quit
+Your To-Do List:
+1. Reading
+2. learning
+3. studying
+PS C:\Users\Janakiraman B\Downloads\Udemy Certificates\Projects\python mini projects> 
