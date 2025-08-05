f6e33c5fc6bbce77721236db673270711e556b18
Janakiraman B <janakiraman171201@gmail.com>
2025-08-05 14:32:07 +0530

    Currencyconventer.py

--- Currencyconventer.py
+++ Currencyconventer.py
@@ -4,4 +4,13 @@ def currency_converter():
     euros = dollars * rate
     print(f"{dollars} USD is equal to {euros} EUR")
 
+
 currency_converter()
+
+
+#OUTPUT
+
+Enter the exchange rate from USD to EUR: 500
+Enter amount in USD: 200
+200.0 USD is equal to 100000.0 EUR
+PS C:\Users\Janakiraman B\Downloads\Udemy Certificates\Projects\python mini projects> 
